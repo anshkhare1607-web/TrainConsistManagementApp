d0a6a87bb18b9de6f74bea90ff987bbb75ac45d8
anshkhare1607-web <anshkhare1607@gmail.com>
2026-03-11 17:57:51 +0530

    Grouping same type of bogies together

--- TrainConsistManagement/src/com/main/Main.java
+++ TrainConsistManagement/src/com/main/Main.java
@@ -1,8 +1,8 @@
 /*
- * UC8: Filter Passenger Bogies Using Streams
+ *  UC9: Group Bogies by Type (Collectors.groupingBy)
  * 
  * @author : Developer
- * version : 8.0
+ * version : 9.0
  * 
  */
 
@@ -15,35 +15,35 @@ import com.trainconsist.*;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Train train = new Train();
 		MainController mainController = new MainController();
-		
+
 		mainController.displayHeader();
 		boolean running = true;
 		// Menu 
 		while(running) {
 			int choice = mainController.showMenuGetChoice(); //getting user choice
-			
+
 			switch(choice) {
 			case 1:
 				mainController.addBogies(train); // adding bogies 
 				break;
 			case 2:
-				mainController.displayUnsorted(train); // unsorted bogies
+				mainController.displayAll(train); // unfiltered bogies
 				break;
 			case 3:
-				mainController.displayFiltered(train); // sorted bogies
+				mainController.displayGrouped(train); // filtered bogies
 				break;
 			case 4:
-					// display final consist
+				// display final consist
 				running = false;
 				break;
 			default:
 				System.out.println("Invalid choice");
 			}
-			
+
 		}
-		
+
 	}
 }
